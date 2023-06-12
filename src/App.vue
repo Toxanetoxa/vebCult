7d71010e82235817cdf0d4f1e138774d252893f0
Antonio Sotnik <toxanetoxa@gmail.com>
2023-06-13 02:34:53 +0300

    all commits

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { RouterView } from "vue-router";
+import { defineComponent } from "vue";
+import VButton from "@/components/ui-components/vButton/vButton.vue";
+import VHeader from "@/components/base-components/vHeader/vHeader.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    VHeader,
+    VButton,
+    RouterView
+  }
+});
+</script>
+
+<template>
+  <v-header />
+  <main class="v-content">
+    <v-button>Привет</v-button>
+    <RouterView />
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+  }
+})
+</script>
+
+<template>
+  <main>
+    1wdasdaweqw
+  </main>
+</template>
